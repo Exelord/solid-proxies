a165b9ccd0fa8407e769a0b5e8fe6acfc2a6372e
Maciej Kwaśniak <contact@exelord.com>
2022-02-20 17:37:13 +0100

    Remove class syntax

--- src/primitives/object.ts
+++ src/primitives/object.ts
@@ -1,31 +1,43 @@
 import { createHandler } from "../handlers/object";
 
-export class SignaledObject<T extends Record<PropertyKey, unknown>> {
-  static fromEntries(entries: Iterable<readonly [PropertyKey, any]>) {
-    return new SignaledObject(Object.fromEntries(entries));
-  }
+interface SignaledObject {
+  fromEntries<T = unknown>(
+    entries: Iterable<readonly [PropertyKey, T]>
+  ): { [key: PropertyKey]: T };
 
-  constructor(obj: T) {
-    let proto = Object.getPrototypeOf(obj);
-    let descriptors = Object.getOwnPropertyDescriptors(obj);
-
-    let clone = Object.create(proto) as T;
-
-    for (let prop in descriptors) {
-      Object.defineProperty(clone, prop, descriptors[prop]);
-    }
-
-    return new Proxy(obj, {
-      ...createHandler<T>(),
-      getPrototypeOf() {
-        return SignaledObject.prototype;
-      },
-    });
-  }
+  new <T extends Record<PropertyKey, unknown> = Record<PropertyKey, unknown>>(
+    obj: T
+  ): T;
 }
+
+export const SignaledObject = function <T extends Record<PropertyKey, unknown>>(
+  obj: T
+): T {
+  let proto = Object.getPrototypeOf(obj);
+  let descriptors = Object.getOwnPropertyDescriptors(obj);
+
+  let clone = Object.create(proto) as T;
+
+  for (let prop in descriptors) {
+    Object.defineProperty(clone, prop, descriptors[prop]);
+  }
+
+  return new Proxy(obj, {
+    ...createHandler<T>(),
+    getPrototypeOf() {
+      return SignaledObject.prototype;
+    },
+  });
+} as any as SignaledObject;
+
+SignaledObject.fromEntries = function <T = unknown>(
+  entries: Iterable<readonly [PropertyKey, T]>
+): { [key: PropertyKey]: T } {
+  return new SignaledObject(Object.fromEntries(entries));
+};
 
 export function createObject<T extends Record<PropertyKey, unknown>>(
   object: T
 ): T {
-  return new SignaledObject(object) as T;
+  return new SignaledObject(object);
 }
