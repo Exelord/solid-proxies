6e67b0e2126e43801022ad963d28d165c87ccaf3
Maciej Kwaśniak <contact@exelord.com>
2022-02-20 11:49:36 +0100

    Init

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+/// <reference types="vitest" />
+
+import path from "path";
+import { defineConfig } from "vite";
+
+module.exports = defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      formats: ["cjs", "es"],
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["solid-js"],
+    },
+  },
+  test: {
+    transformMode: {
+      web: [/\.[jt]s$/],
+    },
+  },
+});
