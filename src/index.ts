5467dd248f743316590f496afbe0d2a527377055
Maciej Kwaśniak <contact@exelord.com>
2022-06-30 21:14:13 +0200

    Fix missing Set export

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from "./primitives/object";
 export * from "./primitives/array";
 export * from "./primitives/map";
 export * from "./primitives/weak-map";
+export * from "./primitives/set";
